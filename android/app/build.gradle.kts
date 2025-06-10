eda4876724c7ea17d683f32e01aafa97aae2aee4
Thacio Alves <thaccio.alves@gmail.com>
2025-06-10 19:56:46 -0300

    login page

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 }
 
 android {
+    ndkVersion = "27.0.12077973"
     namespace = "com.example.educa_mobile"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
